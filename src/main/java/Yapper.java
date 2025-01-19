fced36ea5fc20bbfb9dacef5fd73c05bdf2b9678
mingshan2705 <mingshan270500@gmail.com>
2025-01-19 10:19:57 +0800

    Change Duke to Yapper

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,12 @@
+public class Yapper {
+    public static void main(String[] args) {
+        String message = "____________________________________________________________\n"
+                + " Hello! I'm Yapper!\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(message);
+    }
+}

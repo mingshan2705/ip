10db6a572d1efead0ebe7f855a9b1a0a250122ca
mingshan2705 <mingshan270500@gmail.com>
2025-01-28 10:53:48 +0800

    Ensure coding standard

--- src/main/java/yapper/Parser.java
+++ src/main/java/yapper/Parser.java
@@ -10,24 +10,24 @@ public class Parser {
         String arguments = split.length > 1 ? split[1] : "";
 
         switch (commandWord) {
-            case "bye":
-                return new ExitCommand();
-            case "list":
-                return new ListCommand();
-            case "todo":
-                return new AddTodoCommand(arguments);
-            case "deadline":
-                return new AddDeadlineCommand(arguments);
-            case "event":
-                return new AddEventCommand(arguments);
-            case "delete":
-                return new DeleteCommand(arguments);
-            case "mark":
-                return new MarkCommand(arguments);
-            case "unmark":
-                return new UnmarkCommand(arguments);
-            default:
-                throw new IllegalArgumentException("I'm sorry, I don't understand that command.");
+        case "bye":
+            return new ExitCommand();
+        case "list":
+            return new ListCommand();
+        case "todo":
+            return new AddTodoCommand(arguments);
+        case "deadline":
+            return new AddDeadlineCommand(arguments);
+        case "event":
+            return new AddEventCommand(arguments);
+        case "delete":
+            return new DeleteCommand(arguments);
+        case "mark":
+            return new MarkCommand(arguments);
+        case "unmark":
+            return new UnmarkCommand(arguments);
+        default:
+            throw new IllegalArgumentException("I'm sorry, I don't understand that command.");
         }
     }
 }
